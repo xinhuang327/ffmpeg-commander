0d2a3704c7ea3897206d051cf4ada6ecd4f7c245
Adrian Huang <xinhuang327@gmail.com>
2022-07-20 10:50:33 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
-  root: true,
+	root: false,
 
-  env: {
-    node: true,
-  },
+	env: {
+		node: true,
+	},
 
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+	extends: [
+		'plugin:vue/essential',
+		'@vue/airbnb',
+	],
 
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	},
 
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
 
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 };
